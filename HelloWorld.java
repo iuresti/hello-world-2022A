303585e154594adcbc96a4465e19b60a653b2ea0
Ivan Israel Uresti Adame <ivan.uresti@encora.com>
2022-01-27 08:57:49 -0600

    Un cambio cualquiera

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld{
 		
 		System.out.println("Recibí " + args.length + " parámetros" );
 		
-        System.out.println("Hello World from San Luis Potosí!");
+        System.out.println("Hello World from San Luis Potosí!" + 1231231);
 
 		for(int i = 0; i < args.length; i++){
 			System.out.println(args[i]);
